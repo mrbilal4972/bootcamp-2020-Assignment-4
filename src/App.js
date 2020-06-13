2e18a6d4ff639c364a65ed3cfc3df646a86749e7
mrbilal4972 <mrbilal4972@users.noreply.github.com>
2020-06-13 17:20:52 +0500

    Assignment-4 Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    glow: true,
+    temp: 22
+  });
+
+  function increment(){
+    setState((prevState) => {
+      return {
+        ...state,
+      temp: prevState.temp + 1
+      }
+    })
+  }
+
+  function decrement(){
+    setState((prevState) => {
+      return {
+        ...state,
+      temp: prevState.temp - 1
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${state.glow ? 'glow' : 'off'}`}>
+      <div>
+      <h2>
+      The Light is {state.glow ? 'Glowing' : 'Turned off'}
+      </h2>
+      <button onClick={() => setState({...state, glow: true})}>ON</button>
+      <button onClick={() => setState({...state, glow: false})}>OFF</button>
+      </div>
+      
+      <div className='mt-5'>
+          <button onClick= {increment} type="button" className="btn btn-secondary">+</button>
+          <button type="button" className="btn btn-light" disabled><span className= 'tempText'>Temperture: {state.temp}℃</span></button>
+          <button onClick= {decrement} type="button" className="btn btn-secondary">-</button>
+      </div>
     </div>
   );
 }
